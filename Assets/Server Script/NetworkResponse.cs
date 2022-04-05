d9f7991f7e14f2b3c582ecf1b9be33016f9085d1
Yuting Gao <0711candy@gmail.com>
2022-04-04 17:01:24 -0700

    connected port successfully

---
+++ Assets/Server Script/NetworkResponse.cs
@@ -0,0 +1,11 @@
+using System.IO;
+
+public abstract class NetworkResponse
+{
+
+	public MemoryStream dataStream { get; set; }
+	public short response_id { get; set; }
+
+	public abstract void parse();
+	public abstract ExtendedEventArgs process();
+}
